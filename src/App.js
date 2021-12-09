e75d0746692b93ca63d638c1d62b2f4334b25435
Gary Huang <Gary_Huang@PerfectCorp.com>
2021-12-09 13:29:57 +0800

    chagne app.js code

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
           <br />
-          Hello world!
+          Hello world!1
         </p>
       </div>
     );
